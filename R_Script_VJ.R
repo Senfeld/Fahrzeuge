578bece7a0354710a52ebd939a1a279f0e69d627
Senfeld <vbjanders@gmail.com>
2024-02-04 16:43:32 +0100

    Added function summarize_and_print() that takes data and automatically checks our columns for components.

--- R_Script_VJ.R
+++ R_Script_VJ.R
@@ -1,9 +1,11 @@
 ##### ADD LINES FOR ALL PACKAGES SO THAT ALL GET INSTALLED: I:E: 
 ##### NEEDS TO WORK ON A FRESH START
+##### CREATE VISUALIZATION FOR EACH OEM WITH A data.tree https://cran.r-project.org/web/packages/data.tree/vignettes/data.tree.html
 install.packages("tidyr")
 library(tidyr)
 library(dplyr)
 library(stringr)
+library(purrr)
 
 file_path <- 'Fahrzeug/Fahrzeug/Bestandteile_Fahrzeuge_OEM1_Typ11.csv'
 # Read the CSV fil
@@ -33,87 +35,19 @@ data_motor <- data %>%
             FirstValue = first(ID_Motor))
 data_motor
 
-file_path <- 'Fahrzeug/Fahrzeug/Bestandteile_Fahrzeuge_OEM1_Typ12.csv'
-# Read the CSV fil
-data <- read.csv(file_path, sep=';', header=TRUE)
+summarize_and_print <- function(data, columns) {
+  for (col in columns) {
+    result <- data %>%
+      group_by(Grouped_Column = str_extract(!!sym(col), "^[^-]+")) %>%
+      summarise(Count = n(),
+                FirstValue = first(!!sym(col)))
+    
+    cat("\nSummary for column:", col, "\n")
+    print(result, row.names = FALSE)
+  }
+}
 
-data_karosserie <- data %>%
-  group_by(Grouped_Column = substr(ID_Karosserie, 1, 2)) %>%
-  summarize(Count = n(),
-            FirstValue = first(ID_Karosserie))
-data_karosserie
+columns_to_summarize <- c("ID_Karosserie", "ID_Schaltung", "ID_Sitze", "ID_Motor")
 
-data_schaltung <- data %>%
-  group_by(Grouped_Column = str_extract(ID_Schaltung, "^[^-]+")) %>%
-  summarise(Count = n(),
-            FirstValue = first(ID_Schaltung))
-data_schaltung
-
-data_sitze <- data %>%
-  group_by(Grouped_Column = str_extract(ID_Sitze, "^[^-]+")) %>%
-  summarise(Count = n(),
-            FirstValue = first(ID_Sitze))
-data_sitze
-
-data_motor <- data %>%
-  group_by(Grouped_Column = str_extract(ID_Motor, "^[^-]+")) %>%
-  summarise(Count = n(),
-            FirstValue = first(ID_Motor))
-data_motor
-
-file_path <- 'Fahrzeug/Fahrzeug/Bestandteile_Fahrzeuge_OEM2_Typ21.csv'
-# Read the CSV fil
-data <- read.csv(file_path, sep=';', header=TRUE)
-
-data_karosserie <- data %>%
-  group_by(Grouped_Column = substr(ID_Karosserie, 1, 2)) %>%
-  summarize(Count = n(),
-            FirstValue = first(ID_Karosserie))
-data_karosserie
-
-data_schaltung <- data %>%
-  group_by(Grouped_Column = str_extract(ID_Schaltung, "^[^-]+")) %>%
-  summarise(Count = n(),
-            FirstValue = first(ID_Schaltung))
-data_schaltung
-
-data_sitze <- data %>%
-  group_by(Grouped_Column = str_extract(ID_Sitze, "^[^-]+")) %>%
-  summarise(Count = n(),
-            FirstValue = first(ID_Sitze))
-data_sitze
-
-data_motor <- data %>%
-  group_by(Grouped_Column = str_extract(ID_Motor, "^[^-]+")) %>%
-  summarise(Count = n(),
-            FirstValue = first(ID_Motor))
-data_motor
-
-file_path <- 'Fahrzeug/Fahrzeug/Bestandteile_Fahrzeuge_OEM2_Typ22.csv'
-# Read the CSV fil
-data <- read.csv(file_path, sep=';', header=TRUE)
-
-data_karosserie <- data %>%
-  group_by(Grouped_Column = substr(ID_Karosserie, 1, 2)) %>%
-  summarize(Count = n(),
-            FirstValue = first(ID_Karosserie))
-data_karosserie
-
-data_schaltung <- data %>%
-  group_by(Grouped_Column = str_extract(ID_Schaltung, "^[^-]+")) %>%
-  summarise(Count = n(),
-            FirstValue = first(ID_Schaltung))
-data_schaltung
-
-data_sitze <- data %>%
-  group_by(Grouped_Column = str_extract(ID_Sitze, "^[^-]+")) %>%
-  summarise(Count = n(),
-            FirstValue = first(ID_Sitze))
-data_sitze
-
-data_motor <- data %>%
-  group_by(Grouped_Column = str_extract(ID_Motor, "^[^-]+")) %>%
-  summarise(Count = n(),
-            FirstValue = first(ID_Motor))
-data_motor
-
+# Call the function
+summarize_and_print(data, columns_to_summarize)
